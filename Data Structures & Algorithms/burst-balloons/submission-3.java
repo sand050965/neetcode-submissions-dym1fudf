99f9631c694e085d9983f1d17e556c3323becd7a
Blake Yang <sand050965@gmail.com>
2026-03-30 21:16:41 -0700

    Add: burst-balloons - submission-3

---
+++ Data Structures & Algorithms/burst-balloons/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] newNums = new int[n + 2];
+        System.arraycopy(nums, 0, newNums, 1, n);
+        newNums[0] = newNums[n + 1] = 1;
+        int[][] dp = new int[n + 2][n + 2];
+
+        for (int l = n; l > 0; l--) {
+            for (int r = l; r <= n; r++) {
+                for (int i = l; i <= r; i++) {
+                    int coins = newNums[l - 1] * newNums[i] * newNums[r + 1] + dp[l][i - 1] + dp[i + 1][r];
+                    dp[l][r] = Math.max(dp[l][r], coins);
+                }
+            }
+        }
+
+        return dp[1][n];
+    }
+}
