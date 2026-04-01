46144e5bd7be0d34bc65c49877829001d35f4f23
Blake Yang <sand050965@gmail.com>
2026-03-31 23:06:18 -0700

    Add: partition-labels - submission-3

---
+++ Data Structures & Algorithms/partition-labels/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int n = s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            map.put(ch, i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int l = 0, r = 0;
+        int boundary = -1;
+
+        while (r < n) {
+            char ch = s.charAt(r);
+            boundary = Math.max(boundary, map.get(ch));
+            if (r == boundary) {
+                result.add(r - l + 1);
+                l = r + 1;
+            }
+
+            r++;
+        }
+
+        return result;
+    }
+}
