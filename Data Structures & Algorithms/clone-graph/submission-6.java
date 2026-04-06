b318d39bde02bdf3689a506a59c675f325978c59
Blake Yang <sand050965@gmail.com>
2026-04-06 00:19:47 -0700

    Add: clone-graph - submission-6

---
+++ Data Structures & Algorithms/clone-graph/submission-6.java
@@ -0,0 +1,48 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        dfs(node);
+
+        return map.get(node);
+    }
+
+    private Node dfs(Node node) {
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        Node clone = new Node(node.val);
+        map.putIfAbsent(node, clone);
+
+        for (Node nei : node.neighbors) {
+            clone.neighbors.add(dfs(nei));
+        }
+
+        return clone;
+    }
+}
