1e95c68f44ae8a86838cf53f15ef5fc0d8588d92
Blake Yang <sand050965@gmail.com>
2026-04-05 22:35:58 -0700

    Add: meeting-schedule - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule/submission-7.java
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if (intervals.isEmpty()) {
+            return true;
+        }
+
+        Collections.sort(intervals, (a, b) -> Integer.compare(a.start, b.start));
+        Interval prev = intervals.get(0);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval interval = intervals.get(i);
+            if (Math.max(prev.start, interval.start) < Math.min(prev.end, interval.end)) {
+                return false;
+            }
+            prev = interval;
+        }
+
+        return true;
+    }
+}
