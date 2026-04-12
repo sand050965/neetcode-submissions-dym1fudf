b328520628dfd483b5d29948bfe78da58c687d63
Blake Yang <sand050965@gmail.com>
2026-04-12 15:39:50 -0700

    Add: house-robber - submission-16

---
+++ Data Structures & Algorithms/house-robber/submission-16.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 2];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
+
+        return dp[0];
+    }
+}
