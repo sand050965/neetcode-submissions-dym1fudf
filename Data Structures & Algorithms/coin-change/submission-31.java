daf4120897d67d8ad8259a259b45c9c85d9c86fb
Blake Yang <sand050965@gmail.com>
2026-04-12 16:27:19 -0700

    Add: coin-change - submission-31

---
+++ Data Structures & Algorithms/coin-change/submission-31.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[amount] = 0;
+
+        for (int i = amount - 1; i >= 0; i--) {
+            for (int coin : coins) {
+                if (i > amount - coin || dp[i + coin] == Integer.MAX_VALUE) {
+                    continue;
+                }
+                
+                dp[i] = Math.min(dp[i], 1 + dp[i + coin]);
+            }
+        }
+
+        return (dp[0] == Integer.MAX_VALUE) ? -1 : dp[0];
+    }
+}
