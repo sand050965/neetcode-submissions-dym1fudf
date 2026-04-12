5186c989cea4ca3579d96e31206ce82b135be704
Blake Yang <sand050965@gmail.com>
2026-04-11 17:34:42 -0700

    Add: redundant-connection - submission-14

---
+++ Data Structures & Algorithms/redundant-connection/submission-14.java
@@ -0,0 +1,59 @@
+class Solution {
+    Map<Integer, List<Integer>> adj = new HashMap<>();
+    Set<Integer> visited = new HashSet<>();
+    Set<Integer> cycle = new HashSet<>();
+    int cycleStart = -1;
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        for (int[] edge : edges) {
+            int node1 = edge[0];
+            int node2 = edge[1];
+            adj.computeIfAbsent(node1, k -> new ArrayList<>()).add(node2);
+            adj.computeIfAbsent(node2, k -> new ArrayList<>()).add(node1);
+        }
+
+        dfs(1, -1);
+
+        for (int i = edges.length - 1; i >= 0; i--) {
+            int[] edge = edges[i];
+            int node1 = edge[0];
+            int node2 = edge[1];
+
+            if (cycle.contains(node1) && cycle.contains(node2)) {
+                return edge;
+            }
+        }
+
+        return null;
+    }
+
+    private boolean dfs(int node, int prev) {
+        if (visited.contains(node)) {
+            cycleStart = node;
+            return false;
+        }
+
+        visited.add(node);
+
+        for (int nei : adj.get(node)) {
+            if (nei == prev) {
+                continue;
+            }
+
+            if (!dfs(nei, node)) {
+                if (cycleStart != -1) {
+                    cycle.add(node);
+                }
+
+                if (cycleStart == node) {
+                    cycleStart = -1;
+                }
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
