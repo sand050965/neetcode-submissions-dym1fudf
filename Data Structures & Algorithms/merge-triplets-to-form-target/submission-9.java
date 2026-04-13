49fa11457ab5e366e7ea2b5b4e5c14435d177362
Blake Yang <sand050965@gmail.com>
2026-04-12 20:39:09 -0700

    Add: merge-triplets-to-form-target - submission-9

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-9.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean x = false, y = false, z = false;
+
+        for (int[] triplet : triplets) {
+            x |= (triplet[0] == target[0] && triplet[1] <= target[1] && triplet[2] <= target[2]);
+            y |= (triplet[0] <= target[0] && triplet[1] == target[1] && triplet[2] <= target[2]);
+            z |= (triplet[0] <= target[0] && triplet[1] <= target[1] && triplet[2] == target[2]);
+        }
+
+        return x && y && z;
+    }
+}
