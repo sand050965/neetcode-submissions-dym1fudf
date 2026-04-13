c9ce705a00559ddd943410a963f1b0121d9d324d
Blake Yang <sand050965@gmail.com>
2026-04-12 18:04:32 -0700

    Add: target-sum - submission-17

---
+++ Data Structures & Algorithms/target-sum/submission-17.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer>[] dp = new HashMap[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = new HashMap<>();
+        }
+
+        dp[0].put(0, 1);
+
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+
+            for (Map.Entry<Integer, Integer> entry : dp[i].entrySet()) {
+                int sum = entry.getKey();
+                int count = entry.getValue();
+
+                dp[i + 1].put(sum + num, count + dp[i + 1].getOrDefault(sum + num, 0));
+                dp[i + 1].put(sum - num, count + dp[i + 1].getOrDefault(sum - num, 0));
+            }
+        }
+
+        return dp[n].getOrDefault(target, 0);
+    }
+}
