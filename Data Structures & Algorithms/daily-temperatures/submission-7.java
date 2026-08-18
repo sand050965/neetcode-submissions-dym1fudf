5f5a7bd820dcc7f8cf82cf9ff634e51d3db17cf7
Blake Yang <sand050965@gmail.com>
2026-08-17 21:41:11 -0700

    Add: daily-temperatures - submission-7

---
+++ Data Structures & Algorithms/daily-temperatures/submission-7.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            int temperature = temperatures[i];
+
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperature) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
