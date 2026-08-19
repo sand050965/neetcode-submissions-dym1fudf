000a8fd9f63c05d97830fd0031d4140177d3964b
Blake Yang <sand050965@gmail.com>
2026-08-18 22:56:08 -0700

    Add: trapping-rain-water - submission-15

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-15.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int result = 0;
+        int l = 0, r = n - 1;
+        int leftMax = height[l], rightMax = height[r];
+
+        while (l < r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                result += (leftMax - height[l]);
+            } else {
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                result += (rightMax - height[r]);
+            }
+        }
+
+        return result;
+    }
+}
