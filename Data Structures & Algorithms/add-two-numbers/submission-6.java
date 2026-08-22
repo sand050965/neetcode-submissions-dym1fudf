0a6bde87d4149f38c3bd9c465d6be8c3175e7c25
Blake Yang <sand050965@gmail.com>
2026-08-22 15:02:04 -0700

    Add: add-two-numbers - submission-6

---
+++ Data Structures & Algorithms/add-two-numbers/submission-6.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1), curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 == null) ? 0 : l1.val;
+            int val2 = (l2 == null) ? 0 : l2.val;
+            int sum = val1 + val2 + carry;
+            ListNode node = new ListNode(sum % 10);
+            carry = sum / 10;
+
+            curr.next = node;
+            curr = node;
+            l1 = (l1 == null) ? null : l1.next;
+            l2 = (l2 == null) ? null : l2.next;
+        }
+
+        if (carry != 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        return dummy.next;
+    }
+}
