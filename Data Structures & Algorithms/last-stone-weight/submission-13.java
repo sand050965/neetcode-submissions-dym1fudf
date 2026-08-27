23db708f0becaeac1cf467c8861d8147e8e4665b
Blake Yang <sand050965@gmail.com>
2026-08-27 15:59:28 -0700

    Add: last-stone-weight - submission-13

---
+++ Data Structures & Algorithms/last-stone-weight/submission-13.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int max = Arrays.stream(stones).max().getAsInt();
+        int[] bucket = new int[max + 1];
+        for (int stone : stones) {
+            bucket[stone]++;
+        }
+
+        int first = max, second = first - 1;
+        while (first > 0) {
+            while (first > 0 && bucket[first] == 0) {
+                first--;
+            }
+            
+            bucket[first] %= 2;
+
+            if (first == 0) {
+                return first;
+            }
+
+            if (bucket[first] == 0) {
+                continue;
+            }
+
+            second = first - 1;
+
+            while (second > 0 && bucket[second] == 0) {
+                second--;
+            }
+
+            if (second == 0) {
+                return first;
+            }
+
+            bucket[first]--;
+            bucket[second]--;
+            bucket[first - second]++;
+            first = Math.max(first - 1, first - second);
+        }
+
+        return 0;
+    }
+}
