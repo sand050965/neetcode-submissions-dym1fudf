2ab19fd97fa76e2ae4f0a1f62b2da44165731ab2
Blake Yang <sand050965@gmail.com>
2026-08-28 15:29:31 -0700

    Add: task-scheduling - submission-5

---
+++ Data Structures & Algorithms/task-scheduling/submission-5.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> map = new HashMap<>();
+        Queue<Character> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(map.get(b), map.get(a)));
+        Queue<Pair<Character, Integer>> dq = new ArrayDeque<>();
+        for (char task : tasks) {
+            map.put(task, map.getOrDefault(task, 0) + 1);
+        }
+
+        for (char task : map.keySet()) {
+            maxHeap.offer(task);
+        }
+
+        int time = 0;
+
+        while (!maxHeap.isEmpty() || !dq.isEmpty()) {
+            if (maxHeap.isEmpty()) {
+                time = dq.peek().getValue();
+            }
+
+            while (!dq.isEmpty() && time == dq.peek().getValue()) {
+                Pair<Character, Integer> pair = dq.poll();
+                maxHeap.offer(pair.getKey());
+            }
+
+            char task = maxHeap.poll();
+            map.put(task, map.get(task) - 1);
+            
+            if (map.get(task) > 0) {
+                dq.offer(new Pair<>(task, time + n + 1));
+            }
+
+            time++;
+        }   
+
+        return time;
+    }
+}
