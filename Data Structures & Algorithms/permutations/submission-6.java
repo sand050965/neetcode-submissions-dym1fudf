a831b7c1347b17167dec78bb08264b168208897b
Blake Yang <sand050965@gmail.com>
2026-08-29 15:59:59 -0700

    Add: permutations - submission-6

---
+++ Data Structures & Algorithms/permutations/submission-6.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    Set<Integer> set = new HashSet<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+        backtrack(nums);
+
+        return result;
+    }
+
+    private void backtrack(int[] nums) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(i)) {
+                continue;
+            }
+
+            int num = nums[i];
+            path.add(num);
+            set.add(i);
+            backtrack(nums);
+            path.removeLast();
+            set.remove(i);
+        }
+    }
+}
