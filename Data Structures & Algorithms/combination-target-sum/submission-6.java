c3ef0aacface9cc1f68251e0838e3bb6a9fb882f
Blake Yang <sand050965@gmail.com>
2026-08-29 15:06:17 -0700

    Add: combination-target-sum - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum/submission-6.java
@@ -0,0 +1,29 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        backtrack(nums, target, 0);
+
+        return result;
+    }
+
+    private void backtrack(int[] nums, int target, int idx) {
+        if (target == 0) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        if (idx >= nums.length || target < 0) {
+            return;
+        }
+
+        for (int i = idx; i < nums.length; i++) {
+            int num = nums[i];
+            path.add(num);
+            backtrack(nums, target - num, i);
+            path.removeLast();
+        }
+    }
+}
