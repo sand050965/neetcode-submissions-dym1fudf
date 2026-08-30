30f9e72f0f159a3db3c4c47b2535f15bc6732062
Blake Yang <sand050965@gmail.com>
2026-08-29 22:50:39 -0700

    Add: generate-parentheses - submission-5

---
+++ Data Structures & Algorithms/generate-parentheses/submission-5.java
@@ -0,0 +1,34 @@
+class Solution {
+    int openCnt = 0, closeCnt = 0;
+    List<String> result = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
+    
+    public List<String> generateParenthesis(int n) {
+        backtrack(n);
+
+        return result;
+    }
+
+    private void backtrack(int n) {
+        if (openCnt == n && closeCnt == n) {
+            result.add(sb.toString());
+            return;
+        }
+
+        if (openCnt < n) {
+            sb.append('(');
+            openCnt++;
+            backtrack(n);
+            sb.deleteCharAt(sb.length() - 1);
+            openCnt--;
+        }
+
+        if (closeCnt < openCnt) {
+            sb.append(')');
+            closeCnt++;
+            backtrack(n);
+            sb.deleteCharAt(sb.length() - 1);
+            closeCnt--;
+        }
+    }
+}
