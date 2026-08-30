5f043b618c602615dd1f246f6055bb4d0128f85c
Blake Yang <sand050965@gmail.com>
2026-08-30 13:48:28 -0700

    Add: design-word-search-data-structure - submission-12

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-12.java
@@ -0,0 +1,63 @@
+class TrieNode {
+    Map<Character, TrieNode> children;
+    boolean isEnd;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        isEnd = false;
+    }
+}
+
+class WordDictionary {
+    TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            curr.children.putIfAbsent(ch, new TrieNode());
+            curr = curr.children.get(ch);
+        }
+
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode curr = root;
+
+        return dfs(word, curr, 0);
+    }
+
+    private boolean dfs(String word, TrieNode curr, int idx) {
+        if (idx >= word.length()) {
+            return curr.isEnd;
+        }
+
+        for (int i = idx; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (ch == '.') {
+                for (TrieNode node : curr.children.values()) {
+                    if (dfs(word, node, i + 1)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            } else {
+                if (!curr.children.containsKey(ch)) {
+                    return false;
+                }
+
+                curr = curr.children.get(ch);
+            }
+        }
+
+        return curr.isEnd;
+    }
+}
