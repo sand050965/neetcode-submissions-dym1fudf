49003561ba449790d061045c4327eca94f282d6a
Blake Yang <sand050965@gmail.com>
2026-08-30 18:29:25 -0700

    Add: surrounded-regions - submission-7

---
+++ Data Structures & Algorithms/surrounded-regions/submission-7.java
@@ -0,0 +1,54 @@
+class Solution {
+    int ROWS, COLS;
+    int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public void solve(char[][] board) {
+        ROWS = board.length;
+        COLS = board[0].length;
+
+        for (int r = 0; r < ROWS; r++) {
+            if (board[r][0] == 'O') {
+                dfs(board, r, 0);
+            }
+
+            if (board[r][COLS - 1] == 'O') {
+                dfs(board, r, COLS - 1);
+            }
+        }    
+
+        for (int c = 0; c < COLS; c++) {
+            if (board[0][c] == 'O') {
+                dfs(board, 0, c);
+            }
+
+            if (board[ROWS - 1][c] == 'O') {
+                dfs(board, ROWS - 1, c);
+            }
+        }
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (board[r][c] == 'O') {
+                    board[r][c] = 'X';
+                }
+
+                if (board[r][c] == '*') {
+                    board[r][c] = 'O';
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int r, int c) {
+        if (r < 0 || r >= ROWS || c < 0 || c >= COLS || board[r][c] != 'O') {
+            return;
+        }
+
+        board[r][c] = '*';
+        
+        for (int[] dir : dirs) {
+            int nr = r + dir[0], nc = c + dir[1];
+            dfs(board, nr, nc);
+        }
+    }
+}
