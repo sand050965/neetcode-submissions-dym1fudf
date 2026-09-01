b4272901879093472a298bae3dd55926806ee985
Blake Yang <sand050965@gmail.com>
2026-08-31 22:40:39 -0700

    Add: course-schedule - submission-18

---
+++ Data Structures & Algorithms/course-schedule/submission-18.java
@@ -0,0 +1,43 @@
+class Solution {
+    Map<Integer, List<Integer>> adj = new HashMap<>();
+    Set<Integer> cycle = new HashSet<>();
+    Set<Integer> visited = new HashSet<>();
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        for (int[] prerequisite : prerequisites) {
+            int course = prerequisite[0], pre = prerequisite[1];
+            adj.computeIfAbsent(pre, k -> new ArrayList<>()).add(course);
+        }    
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean dfs(int course) {
+        if (cycle.contains(course)) {
+            return false;
+        }
+
+        if (visited.contains(course)) {
+            return true;
+        }
+
+        cycle.add(course);
+
+        for (int nei : adj.getOrDefault(course, new ArrayList<>())) {
+            if (!dfs(nei)) {
+                return false;
+            }
+        }
+
+        cycle.remove(course);
+        visited.add(course);
+
+        return true;
+    }
+}
