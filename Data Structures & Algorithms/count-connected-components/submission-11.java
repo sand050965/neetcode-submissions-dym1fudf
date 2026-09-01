393155e51ebc7857e49ceb892b0c2d4c814fef6d
Blake Yang <sand050965@gmail.com>
2026-08-31 23:24:39 -0700

    Add: count-connected-components - submission-11

---
+++ Data Structures & Algorithms/count-connected-components/submission-11.java
@@ -0,0 +1,41 @@
+class Solution {
+    Map<Integer, List<Integer>> adj = new HashMap<>();
+    Set<Integer> visited = new HashSet<>();
+
+    public int countComponents(int n, int[][] edges) {
+        int result = 0;
+
+        for (int[] edge : edges) {
+            int n1 = edge[0], n2 = edge[1];
+            adj.computeIfAbsent(n1, k -> new ArrayList<>()).add(n2);
+            adj.computeIfAbsent(n2, k -> new ArrayList<>()).add(n1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited.contains(i)) {
+                continue;
+            }
+
+            dfs(i, -1);
+            result++;
+        }
+
+        return result;
+    }
+
+    private void dfs(int node, int parent) {
+        if (visited.contains(node)) {
+            return;
+        }
+
+        visited.add(node);
+        
+        for (int nei : adj.getOrDefault(node, new ArrayList<>())) {
+            if (nei == parent) {
+                continue;
+            }
+
+            dfs(nei, node);
+        }
+    }
+}
