1c86903caa15752b0f3c1d49fb56d2bb62398e3c
Blake Yang <sand050965@gmail.com>
2026-09-01 15:48:53 -0700

    Add: redundant-connection - submission-19

---
+++ Data Structures & Algorithms/redundant-connection/submission-19.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        Queue<Integer> dq = new ArrayDeque<>();
+        int[] indegree = new int[n + 1];
+
+        for (int[] edge : edges) {
+            int n1 = edge[0], n2 = edge[1];
+            adj.computeIfAbsent(n1, k -> new ArrayList<>()).add(n2);
+            adj.computeIfAbsent(n2, k -> new ArrayList<>()).add(n1);
+            indegree[n1]++;
+            indegree[n2]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 1) {
+                dq.offer(i);
+            }
+        }
+
+        while (!dq.isEmpty()) {
+            int node = dq.poll();
+            indegree[node]--;
+
+            for (int nei : adj.get(node)) {
+                indegree[nei]--;
+
+                if (indegree[nei] == 1) {
+                    dq.offer(nei);
+                }
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int[] edge = edges[i];
+            int n1 = edge[0], n2 = edge[1];
+
+            if (indegree[n1] == 2 && indegree[n2] == 2) {
+                return edge;
+            }
+        }
+
+        return new int[0];
+    }
+}
