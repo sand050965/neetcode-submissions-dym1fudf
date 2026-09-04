260cfd929695bd5a961d3d5f51f4db4e93540b03
Blake Yang <sand050965@gmail.com>
2026-09-03 22:55:07 -0700

    Add: clone-graph - submission-11

---
+++ Data Structures & Algorithms/clone-graph/submission-11.java
@@ -0,0 +1,44 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        map.put(null, null);
+
+        return dfs(node);    
+    }
+
+    private Node dfs(Node node) {
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        Node copyNode = new Node(node.val);
+        map.put(node, copyNode);
+
+        for (Node nei : node.neighbors) {
+            copyNode.neighbors.add(dfs(nei));
+        }
+
+        return copyNode;
+    }
+}
