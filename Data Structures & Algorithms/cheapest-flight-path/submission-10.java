5055ed7cff0a3daad3361e8fe53d472e2c81c99a
Blake Yang <sand050965@gmail.com>
2026-09-08 15:54:16 -0700

    Add: cheapest-flight-path - submission-10

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-10.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int cnt = 0;
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        Deque<int[]> dq = new ArrayDeque<>();
+        int[] prices = new int[n];
+        Arrays.fill(prices, Integer.MAX_VALUE);
+        prices[src] = 0;
+
+        for (int[] flight : flights) {
+            int s = flight[0], d = flight[1], p = flight[2];
+            adj.computeIfAbsent(s, key -> new ArrayList<>()).add(new int[] {d, p});
+        }
+
+        dq.offer(new int[] {src, 0});
+
+        while (!dq.isEmpty() && cnt < k + 1) {
+            for (int i = dq.size(); i > 0; i--) {
+                int[] curr = dq.poll();
+                int node = curr[0], p = curr[1];
+
+                for (int[] nei : adj.getOrDefault(node, new ArrayList<>())) {
+                    int neiNode = nei[0], neiP = nei[1];
+                    if (p + neiP < prices[neiNode]) {
+                        prices[neiNode] = p + neiP;
+                        dq.offer(new int[] {neiNode, p + neiP});
+                    }
+                }
+            }
+            cnt++;
+        }
+
+        return (prices[dst] == Integer.MAX_VALUE) ? -1 : prices[dst];
+    }
+}
