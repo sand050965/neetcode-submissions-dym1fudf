1d4ff94965e21808a52808af61244d6cef94a2c6
Blake Yang <sand050965@gmail.com>
2026-09-08 17:11:06 -0700

    Add: reconstruct-flight-path - submission-32

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-32.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> adj = new HashMap<>();
+        Stack<String> stack = new Stack<>();
+        LinkedList<String> result = new LinkedList<>();
+
+        for (List<String> ticket : tickets) {
+            String from = ticket.get(0), to = ticket.get(1);
+            adj.computeIfAbsent(from, k -> new PriorityQueue<>((a, b) -> a.compareTo(b))).offer(to);
+        }
+
+        stack.push("JFK");
+        while (!stack.isEmpty()) {
+            String node = stack.peek();
+
+            if (!adj.containsKey(node) || adj.get(node).isEmpty()) {
+                result.addFirst(stack.pop());
+                continue;
+            }
+
+            stack.push(adj.get(node).poll());
+        }
+
+        return result;
+    }
+}
