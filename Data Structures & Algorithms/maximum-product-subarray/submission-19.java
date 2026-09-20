926cdc7fc03d019d45589cfc6edeb43edaa294b6
Blake Yang <sand050965@gmail.com>
2026-09-19 18:26:31 -0700

    Add: maximum-product-subarray - submission-19

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-19.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int minProduct = 1, maxProduct = 1, result = nums[0];
+        for (int num : nums) {
+            int minProductCopy = minProduct, maxProductCopy = maxProduct;
+            minProduct = Math.min(num, Math.min(num * minProductCopy, num * maxProductCopy));
+            maxProduct = Math.max(num, Math.max(num * minProductCopy, num * maxProductCopy));
+            result = Math.max(result, maxProduct);
+        }
+
+        return result;
+    }
+}
